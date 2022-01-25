e0f5567fdaec46830fd2c6581c50e3b0da566181
remi-vieille <remivieille13@gmail.com>
2022-01-25 18:34:52 +0100

    creact react app and index initialised

---
+++ client/src/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import './styles/index.scss';
+// import App from './components/App';
+
+
+ReactDOM.render(
+  <React.StrictMode>
+    {/* <App /> */}
+    <h1>salut</h1>
+  </React.StrictMode>,
+  document.getElementById('root')
+);
